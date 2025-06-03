0c21d9837424f6d1d3db88f90cfc4567c670300c
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-06-02 22:59:54 -0300

    Update app.js

--- JavaScript/section16/conditionals/app.js
+++ JavaScript/section16/conditionals/app.js
@@ -5,3 +5,15 @@ if (random < 0,5){
     console.log(random)
 }
 console.log("After conditional")
+
+const age = 4;
+
+if (age < 5){
+    console.log("You are a baby")
+} else if( age <= 10){
+    console.log("you are a child")
+} else if (age > 10){
+    console.log("You are a adult")
+} else if (age >= 65){
+    console.log("You are a senior")
+}
