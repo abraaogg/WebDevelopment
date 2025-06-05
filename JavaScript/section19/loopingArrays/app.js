9d9a8d60675648cd762d009ad01fff86782864f7
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-06-05 14:15:34 -0300

    looping arrays

---
+++ JavaScript/section19/loopingArrays/app.js
@@ -0,0 +1,30 @@
+const animals = [
+  "Bear",
+  "Cheetah",
+  "Dolphin",
+  "Eagle",
+  "Elephant",
+  "Fox",
+  "Giraffe",
+  "Hippopotamus",
+  "Kangaroo",
+  "Koala",
+  "Leopard",
+  "Lion",
+  "Otter",
+  "Panda",
+  "Penguin",
+  "Raccoon",
+  "Shark",
+  "Tiger",
+  "Wolf",
+  "Zebra"
+];
+
+for (let i = 0; i < animals.length; i++){
+    console.log(i, animals[i])
+}
+
+for (let i = animals.length - 1; i >= 0 ; i--){
+    console.log(i, animals[i])
+}
