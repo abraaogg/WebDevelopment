36a6793d16d06a71ddb1998c9b350cd84330ad11
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-06-11 16:01:14 -0300

    pokemon

--- JavaScript/DOM/section24/app.js
+++ JavaScript/DOM/section24/app.js
@@ -6,6 +6,8 @@ for (let img of allImages){
 
 const squareImages = document.getElementsByClassName('square');
 
-for  ( let img of squareImages){
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+
+const allLinks = document.querySelectorAll('a');
+for (let link of allLinks){
+    link.style.color = 'cyan'
 }
