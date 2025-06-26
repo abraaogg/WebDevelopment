b052d43dcf5bc69a47e1cab03864f268f006a081
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-06-26 14:39:22 -0300

    Update index.js

--- NodeJs/FirstApp/index.js
+++ NodeJs/FirstApp/index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express()
 console.dir(app)
+
+app.use((req, res) =>{
+    console.log('We got a new request!')
+    res.send('Hello, we got your request!')
+})
+app.listen(3000, () =>{
+
+})
