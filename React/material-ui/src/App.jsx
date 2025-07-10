9fc2540f6c4830463ad1a6ac36efc79daab16400
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-07-10 17:44:07 -0300

    section72

---
+++ React/material-ui/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from '@mui/material/Button';
+import RatingDemo from './RatingDemo';
+
+function App() {
+
+  return (
+    <>
+      <RatingDemo/>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  )
+}
+
+export default App
